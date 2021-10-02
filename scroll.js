e53b501db757738e3dc9d90f15b896edb0fa2243
Jhonier16 <91768612+Jhonier16s@users.noreply.github.com>
2021-10-02 16:41:07 -0500

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+ 
+    sr.reveal('.navbar', {
+
+        duration: 2000,
+        origin : 'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('', {
+
+        duration: 2000,
+        origin : 'left',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-nosotros', {
+
+        duration: 4000,
+        origin: 'bottom',
+        distance: '400px'
+    });
+    sr.reveal('.scroll-footer', {
+
+        duration: 3000,
+        origin : 'left',
+        distance: '400px'
+    });
+
+    sr.reveal('.scroll-menu', {
+
+        duration: 3000,
+        origin : 'left',
+        distance: '400px'
+    });
